fab9bded29932d11c0d05df92483755a04c60c08
vtb7 <88791316+vtb7@users.noreply.github.com>
2021-09-04 14:14:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function draw() 
+{
+background("green");
+if(keyDown("left")) {
+  background("yellow");
+}
+if(keyDown("right")) {
+  background("red")
+}
+if(keyDown("up")) {
+  background("blue")
+}
+if(keyDown("down")) {
+  background("lightblue");
+}
+}
+
